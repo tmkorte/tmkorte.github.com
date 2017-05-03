d8e87c607470146e495c534e16efe9aa2f311f53
Mike Korte <korte@rivet.works>
2017-05-03 15:32:41 -0500

    Add sidebar nav
    Signed-off-by: Mike Korte <korte@rivet.works>

---
+++ _documentation/submission-api/index copy.md
@@ -0,0 +1,5 @@
+---
+layout: page
+title: Rivet Submission API
+---
+This is the Submission API
---
+++ assets/js/documentation.js
@@ -0,0 +1,20 @@
+$('.doc-body').scrollNav({
+    sections: 'h2, h3',
+    subSections: 'h4',
+    sectionElem: 'section',
+    showHeadline: false,
+    headlineText: 'Scroll To',
+    showTopLink: false,
+    topLinkText: 'Top',
+    fixedMargin: 40,
+    scrollOffset: 40,
+    animated: true,
+    speed: 500,
+    insertTarget: '#doc-menu',
+    insertLocation: 'prependTo',
+    arrowKeys: false,
+    scrollToHash: true,
+    onInit: null,
+    onRender: null,
+    onDestroy: null
+});
